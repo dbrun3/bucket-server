0e21d83cd2b35de3b094ae23f8242692c5e3aaa3
Dylan Brunelle <dylan.brunelle@barchart.com>
2025-09-27 18:22:31 -0500

    add healthcheck

--- main.go
+++ main.go
@@ -15,8 +15,14 @@ func main() {
 	if err != nil {
 		log.Fatal("Fatal error reading config: ", err)
 	}
+
 	handler := handler.NewHandler(*config)
 	mux.Handle("/", handler)
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	log.Println("Server Listening on 8080")
 	http.ListenAndServe(":8080", mux)
